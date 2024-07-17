dd0cafed0e9a453b2fa0614bdf2ef398ac9b38c2
yood2 <yood2@proton.me>
2024-07-17 14:21:40 -0700

    Project update

--- complete_intro_to_web_dev_v3/servers/wordle.js
+++ complete_intro_to_web_dev_v3/servers/wordle.js
@@ -1,28 +1,44 @@
 let url = "https://words.dev-apis.com/word-of-the-day"
 
-let first_guess = ""
-let second_guess = ""
-let third_guess = ""
-let fourth_guess = ""
-let fifth_guess = ""
-let sixth_guess = ""
+let complete = false
+
+const display = document.querySelector(".first-guess")
+let displayStr = ""
 
 async function getWord() {
     const promise = await fetch(url)
     const processed = await promise.json()
     displayStr = processed.word
-    display.innerText = displayStr;
+    display.innerText = displayStr
+}
+
+function scoreboard(event) {
+    if (!complete) {
+        if (displayStr.length < 6) {
+            displayStr += event.key
+            console.log(displayStr)
+            display.innerText = displayStr
+        }
+        console.log("Too long")
+    }
 }
 
 function buttonClick() {
     let str = getWord()
 }
 
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
+}
+
 function init() {
     document
       .addEventListener("keydown", function (event) {
-
-        console.log("Pressed: " + event.key)
+        if (!complete) {
+            if (isLetter(event.key)) {
+                scoreboard(event)
+            }
+        }
       });
   }
   
