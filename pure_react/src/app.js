dc6240d3e74891aa6158cda2a16264f37670e39f
yood2 <yood2@proton.me>
2024-07-27 21:48:23 -0700

    Pure React

---
+++ pure_react/src/app.js
@@ -0,0 +1,34 @@
+const Pet = (param) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, param.name),
+        React.createElement("h2", {}, param.animal),
+        React.createElement("h2", {}, param.breed),
+    ]);
+};
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese",
+        }),
+        React.createElement(Pet, {
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockatiel",
+        }),
+        React.createElement(Pet, {
+            name: "Doink",
+            animal: "Cat",
+            breed: "Mixed",
+        })
+    );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
