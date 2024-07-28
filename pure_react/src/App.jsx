c4e4d99aac26bd6958c9bf9d34fcd2ee47ee4f03
yood2 <yood2@proton.me>
2024-07-27 22:41:24 -0700

    Installed Vite, adjusted settings in package.json

--- pure_react/src/App.jsx
+++ pure_react/src/App.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom';
+
 const Pet = (param) => {
     return React.createElement('div', {}, [
         React.createElement('h1', {}, param.name),
@@ -30,5 +33,5 @@ const App = () => {
 };
 
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
