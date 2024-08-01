7ca1106af786d7cb72813446a95de8f9b0e6e214
yood2 <yood2@proton.me>
2024-08-01 16:06:03 -0700

    Implement API calls using react query, refactor BreedList and Pet components to have separate fetch components using React Query

--- intro-to-react-v8/src/App.jsx
+++ intro-to-react-v8/src/App.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            cacheTime: Infinity,
+        },
+    },
+});
+
 import Details from './Details';
 import SearchParams from './SearchParams';
 
 const App = () => {
     return (
         <div>
-            <BrowserRouter>
-                <header>
-                    <Link to="/">Adopt Me!</Link>
-                </header>
-                <Routes>
-                    <Route path="/details/:id" element={<Details />} />
-                    <Route path="/" element={<SearchParams />} />
-                </Routes>
-            </BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <header>
+                        <Link to="/">Adopt Me!</Link>
+                    </header>
+                    <Routes>
+                        <Route path="/details/:id" element={<Details />} />
+                        <Route path="/" element={<SearchParams />} />
+                    </Routes>
+                </BrowserRouter>
+            </QueryClientProvider>
         </div>
     );
 };
