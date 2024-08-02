0ae8cb589e40f162240397e76f24b607cc6fade2
yood2 <yood2@proton.me>
2024-08-02 13:05:01 -0700

    Added Modal functionality by creating a portal to a div in the index.html that is above all other components. Used for confirmation screen in the Details component.

--- intro-to-react-v8/src/Details.jsx
+++ intro-to-react-v8/src/Details.jsx
@@ -1,5 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import Modal from './Modal';
 import fetchPet from './fetchPet';
 import Carousel from './Carousel';
 import ErrorBoundary from './ErrorBoundary';
@@ -7,6 +9,7 @@ import ErrorBoundary from './ErrorBoundary';
 const Details = () => {
     const { id } = useParams();
     const results = useQuery(['details', id], fetchPet);
+    const [showModal, setShowModal] = useState(false);
 
     if (results.isLoading) {
         return (
@@ -24,8 +27,23 @@ const Details = () => {
                 <Carousel images={pet.images} />
                 <h1>{pet.name}</h1>
                 <h2>{`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
-                <button>Adopt {pet.name}</button>
+                <button onClick={() => setShowModal(true)}>
+                    Adopt {pet.name}
+                </button>
                 <p>{pet.description}</p>
+                {showModal ? (
+                    <Modal>
+                        <div>
+                            <h1>Would you like to adopt {pet.name}?</h1>
+                            <div className="buttons">
+                                <button>Yes</button>
+                                <button onClick={() => setShowModal()}>
+                                    No
+                                </button>
+                            </div>
+                        </div>
+                    </Modal>
+                ) : null}
             </div>
         </div>
     );
